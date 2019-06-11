8a1cfdab915a6f39dafcdfff270dd0236a6f6da3
zowong5963 <zowong@uci.edu>
2019-06-11 09:58:27 -0700

    added comments

--- looper/src/PlayWindow/PlayWindow.js
+++ looper/src/PlayWindow/PlayWindow.js
@@ -10,27 +10,31 @@ import Microphone from './InstrumentPlayer/Instruments/Microphone';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
+// Renders the instructions
 const DisplayInstrumentInstructions = ({ playWindowState }) => {
     return <div className="m-5">
         <h3>Instructions:</h3>
         <div>
-            <p>Press any of the above keys to play the indicated sound. Begin recording input by pressing the "Record" button.
+            <p>
+                Press any of the above keys to play the indicated sound. Begin recording input by pressing the "Record" button.
                 Stop recording by pressing the "Stop Recording" button. Recorded tracks can be played individually by clicking 
                 on the track icons (which will appear after making a recording). Clicking the "Play All" button will play all
-                tracks simultaneously. Playback can be stopped by clicking the "Stop Playback" button. 
+                tracks simultaneously. Playback can be stopped by clicking the "Stop Playback" button. Tracks can be deleted
+                by clicking on the red "X" button next to each track. 
             </p>
         </div>
     </div>
 };
 
-
+// Sleep function used to make the looper wait before playing the next note in a track
 const sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
   }
 
-
+// Global variable that indicates the user's choice to stop playback
 var callToStop = false;
 
+// Renders the recorded tracks
 const DisplayTracks = ({ playWindowState, removeTrack}) => {
     return <div>
         {playWindowState.trackList.map((track, index) => {
@@ -45,37 +49,42 @@ const DisplayTracks = ({ playWindowState, removeTrack}) => {
     </div>
 }
 
+// Asynchronous function that loops through and plays a recorded track
 const playTrack = async (track, stopTime) => {
     var firstTime = 0;
     var hitFirst = false;
     var waitTime = 0;
     callToStop = false;
     while (true) {
+        // Checks every loop through to see if the user has clicked the "Stop Playback" button
         if (callToStop) {
             return;
         }
-        if (track.id !== 'Microphone') {
+        if (track.id !== 'Microphone') { // If recorded instrument is not microphone
             track.data.forEach(value => {
-                if (value.currentAudioFile) {
+                if (value.currentAudioFile) { // Loop through each sound in the track and play sequentially
                     waitTime = value.timeDiff;
                     if (!hitFirst) {
                         hitFirst = true;
                         firstTime = value.timeStamp;
                         waitTime = 0;
                     }
-                    sleep(waitTime).then(() => {
+                    sleep(waitTime).then(() => { // Wait the appropriate amount of time before playing next sound in track
                         value.currentAudioFile.cloneNode(true).play();
                     });
                 }
             });
+            // Don't replay track until it has finished playing
             var promise = new Promise(resolve => {
                 setTimeout(() => resolve("done"), stopTime - firstTime);
             })
             await promise;
         }
-        else {
-            const audio = new Audio(track.data[0].blobURL);
+        else { // If recorded instrument is a microphone
+            // Store and play blob object holding microphone input
+            const audio = new Audio(track.data[0].blobURL); 
             audio.play();
+            // Don't replay track until it has finished playing
             var promise = new Promise(resolve => {
                 setTimeout(() => resolve("done"), track.data[0].stopTime - track.data[0].startTime);
             });
@@ -121,7 +130,8 @@ class PlayWindow extends React.Component {
         }
     }
 
-    removeTrack =  (index) => {
+    // Remove selected track
+    removeTrack = (index) => {
         if (index > -1) {
             this.state.trackList.splice(index, 1);
         }
@@ -143,6 +153,7 @@ class PlayWindow extends React.Component {
         });
     }
 
+    // Play all recorded tracks
     playAll = () => {
         this.trackList.forEach(x => playTrack(x, x.stopTime));
     }
@@ -180,34 +191,7 @@ class PlayWindow extends React.Component {
         });
     }
 
-    trackVerification = (trackListToVerify, trackIndex) => {
-        return trackIndex < trackListToVerify.length
-            && trackListToVerify[trackIndex]
-            && trackListToVerify[trackIndex].currentAudioFile;
-    }
-
-    playAllTracks = () => {
-        for (let i = 0; i < 10; i += 1) {
-            if (this.trackVerification(this.instruments[0].trackList, i)) {
-                this.instruments[0].trackList[i].currentAudioFile.cloneNode(true).play();
-            }
-
-            if (this.trackVerification(this.instruments[1].trackList, i)) {
-                this.instruments[1].trackList[i].currentAudioFile.cloneNode(true).play();
-            }
-
-            if (this.trackVerification(this.instruments[3].trackList, i)) {
-                this.instruments[3].trackList[i].currentAudioFile.cloneNode(true).play();
-            }
-
-            if (this.trackVerification(this.instruments[4].trackList, i)) {
-                this.instruments[4].trackList[i].currentAudioFile.cloneNode(true).play();
-            }
-
-        }
-    }
-
-
+    // Renders the "Record", "Play All", and "Stop Playback" buttons
     render() {
         return (<div className="container-fluid">
             <DashBoard changeInstrument={this.updateInstrument} instruments={this.instruments}></DashBoard>
